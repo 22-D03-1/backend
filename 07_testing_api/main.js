717a53f043d74c2f21e30648efe6b8423638368d
Ferdi <hoskeee@gmail.com>
2022-12-14 15:38:22 +0100

    Comments API Testing

--- 07_testing_api/main.js
+++ 07_testing_api/main.js
@@ -5,14 +5,19 @@ import hotelRouter from "./routes/hotelRoutes.js"
 const server = express()
 const port = 4000
 
-//SO NIE
+/**
+ * Das ist natürlich eine sehr schlechte und unsichere Security Maßnahme.
+ * Wie wir unser Backend sicherer machen, lernen wir in ein paar Wochen.
+ * Das ist aber zur Veranschaulichung eine Middleware, die überprüft, ob ein API Key
+ * über die Query Parameter übergeben wurde und ob dieser stimmt.
+ */
 const apiKey = "040"
 
 server.use((req, res, next) => {
     if(req.query.api_key === apiKey) {
         next()
     } else {
-        res.status(401).send("Du kommst hier nicht rein. Hau ab")
+        res.status(401).send("Du kommst hier nicht rein.")
     }
 })
 
--- 07_testing_api/tests/routes.test.js
+++ 07_testing_api/tests/routes.test.js
@@ -2,21 +2,57 @@ import server from "../main.js"
 import request from "supertest"
 
 /**
- * Supertest
+ * Während unserer Entwicklung, ist unsere test.js Datei die erste Anlaufstelle.
+ * Wir überlegen uns erst, was soll unsere können und definieren anschließend
+ * den dazugehörigen Test. Bspw wollen wir nach einem erfolgreichen POST wir den 
+ * Statuscode 201 zurück bekommen möchten. Also testen wir ebendas.
+ * Wenn der Test geschrieben ist, sollten wir einen Timeout zurück bekommen,
+ * da die Route noch nicht definiert ist. Wir bauen dann in unserem Controller nur das,
+ * dass unser Test erfolgreich ist.
  */
 
 describe("Hotel Routes Test", ()=>{
+    /**
+     * Wir können beliebig viele describe ineinader verschachteln. Hier erstellen wir mit
+     * describe erst eine Gruppe für alle Hotel Routen, dann erstellen wir mit describe eine
+     * weitere Gruppe für alle Tests für die spezifische Route. So ist bei Ausführen des
+     * Tests übersichtlicher welche Tests erfolgreich und welche nicht waren.
+     */
     describe("Get All Route", ()=>{
         let response = {}
         
-        /** */
-        beforeAll(async ()=>{
+        /**
+         * Innerhalb unserer Gruppe können wir mit der beforeAll Methode Code ausführen, der vor
+         * dem Laufen der Tests ausgeführt wird. Bspw der Request muss für jeden Test gemacht werden.
+         * Also führen wir ihn vor allen Tests aus und können in jedem Test auf die response zugreifen.
+         * Aufgepasst: Wenn sich die response ändert bpsw. Wenn wir testen wollen, was passiert wenn wir
+         * einen falschen Parameter übergeben, müssen wir diesen doch innerhalb des Tests ausführen.
+         */
+        beforeAll(async ()=> {
+            /**
+             * Zum ausführen unserer Request nutzen wir das Paket supertest. Supertest funktioniert ähnlich
+             * wie fetch mit dem Unterschied, dass unser Backend parallel nicht laufen muss. Dafür übergeben
+             * wir unser server Objekt aus der main.js und können dann mit .get(), .post() etc. unsere
+             * Requests machen.
+             */
             response = await request(server).get("/hotels/?api_key=040")
         })
+        /**
+         * Ein Standardtest für eine Route: Bekomme ich den richtigen Statuscode zurück
+         * Reminder: 200 für erfolgreichen GET; 201 für erfolgreichen POST; 202 für erfolreichen PUT;
+         * 404 wenn nichts gefunden wird usw...
+         */
+
         //Unsere Route gibt Statuscode 200 zurück
         test("return 200", () => {
             expect(response.statusCode).toBe(200)
         })
+
+        /**
+         * Wir können alles mögliche Testen wie hier bspw ob wir ein JSON erhalten. Hier ist die Balnce wichtig:
+         * Nicht zu wenige Tests damit wir alles wichtige probieren, nicht zu viele und der Test zu langsam und
+         * unübersichtlich wird
+         */
 
         //Unsere Route gibt ein JSON zurück
         test("return JSON", () => {
